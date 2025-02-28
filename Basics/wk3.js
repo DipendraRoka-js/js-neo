4346c7735a4dc03252aeb302e47555406b40d8e7
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2025-02-28 16:51:11 +0000

    hehe

---
+++ Basics/wk3.js
@@ -0,0 +1,21 @@
+let value = 3
+let negativenum = -value
+
+// console.log(negativenum);
+ let str1 = "dipendra"
+ let str2 = "dipendra"
+
+ let str3 =str1 + str2
+//  console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+ 
+let preFixed = 100
+let y = ++preFixed
+// ++preFixed
+// console.log(preFixed);
+// console.log(y);
+
