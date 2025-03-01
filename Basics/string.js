91e8c59e39ce0cc49714f12690e2edad9e93d55c
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2025-03-01 04:21:37 +0000

    String property

---
+++ Basics/string.js
@@ -0,0 +1,11 @@
+const name = "Dipen"
+const repocount = 50
+
+// console.log(name + repocount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is${repocount}`);
+
+const deeperString = new String('dipendraroka');
+
+console.log(deeperString[0]);
+
