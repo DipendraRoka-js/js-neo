3541a1b9bb15e3f7cf429bec5ee9ec68b5a0d92a
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2025-03-01 10:39:20 +0000

    string and its properties

--- Basics/string.js
+++ Basics/string.js
@@ -3,9 +3,34 @@ const repocount = 50
 
 // console.log(name + repocount + "Value");
 
-console.log(`Hello my name is ${name} and my repo count is${repocount}`);
+// console.log(`Hello my name is ${name} and my repo count is${repocount}`);
 
-const deeperString = new String('dipendraroka');
+const deeperString = new String('dip_endra_roka');
 
-console.log(deeperString[0]);
+// console.log(deeperString[0]);
+// console.log(deeperString.__proto__);
+
+// console.log(deeperString.length);
+// console.log(deeperString.toUpperCase());
+// console.log(deeperString.charAt(5));
+// console.log(deeperString.indexOf('r'));
+
+const neSt = deeperString.substring(0,3)
+console.log(neSt);
+
+// const Diop = deeperString.slice(-8,4)
+// console.log(Diop);
+
+const newStringOne = "  gopeer. "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url ="https://fsfsffsf.com/deeffs%453choudhary"
+console.log(url.replace('%453','-'));
+
+console.log(deeperString.split('_'));
+
+
+
 
