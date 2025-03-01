84780a46ef1f05e67e6db680ce4e7e5705689947
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2025-03-01 04:07:08 +0000

    heap and stack

---
+++ Basics/memory.js
@@ -0,0 +1,23 @@
+//Stack(Primitive), Heap(Non-primitive)
+
+let fog = "Erorr"
+let gop = fog
+
+gop = "heheeh"
+
+console.log(fog);
+console.log(gop);
+
+
+let userOne ={
+    emai:"sfsdfsd@gmail.com",
+    upi:"fsdfsd@"
+}
+
+let userTwo = userOne
+
+userTwo.emai = "sfsdfferreee@gmail.com"
+
+console.log(userOne.emai);
+console.log(userTwo.emai);
+
