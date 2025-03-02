225c3848c69cb31ee561927599706667cb3a6c18
Dipendra Roka <np03cs4a230497@heraldcollege.edu.np>
2025-03-02 15:54:55 +0000

    Math is easy learning about random and its fucntion

---
+++ Basics/number.js
@@ -0,0 +1,36 @@
+const geppo = new Number(10)
+// console.log(geppo);
+
+// console.log(geppo.toString().length);
+// console.log(geppo.toFixed(1));
+
+// const otherNumber = 24.8922
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+
+
+//++++++++++++++++Mathematics operation++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
+// console.log(Math.pow(3,4));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min = 10
+const amx = 20
+
+console.log(Math.floor(Math.random() * (amx - min + 1)) + min)
+
+
+
+
+
