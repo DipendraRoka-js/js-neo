e43983011df7a37320eade63da432eefde9acf69
Dipendra Roka <np03cs4a230497@heraldcollege.edu.np>
2025-03-03 15:53:22 +0000

    this is aray

---
+++ Basics/areee.js
@@ -0,0 +1,22 @@
+//Arrays
+const myArray = [1,2,3,4,5,5]
+// console.log(myArray[5]);
+// myArray.push(43);
+// console.log(myArray.includes(9));
+// const newARR = myArray.join();
+// console.log(myArray);
+// console.log(typeof newARR);
+
+console.log("A ",myArray);
+
+const myn1 = myArray.slice(1,3);
+
+console.log(myn1);
+console.log("B ",myArray);
+
+const myn2 = myArray.splice(1,3);
+console.log("C",myArray);
+console.log(myn2);
+
+
+
