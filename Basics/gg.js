4e2c39fdcd8843c7fa64d7923ed48a6eff2e5dee
Dipendra Roka <np03cs4a230497@heraldcollege.edu.np>
2025-03-05 12:49:31 +0000

    this is areeeee

--- Basics/gg.js
+++ Basics/gg.js
@@ -1,3 +1,29 @@
 //hello
 
-console.log("helloo");
+const arrayss = ["dipendra","hello","hoe"]
+const ray = ["gope","fsdf","fsdfs"]
+
+// const deeper =  arrayss.concat(ray);
+// console.log(deeper);
+
+// const all_new_heroes = [...arrayss, ...ray]
+
+const hole = [1,2,3,[4,5,6],7,[3,4,[9,7]]]
+
+const real_another_array = hole.flat(Infinity)
+// console.log(real_another_array);
+
+
+// console.log(Array.isArray("dIPENDRA"));
+// console.log(Array.from("dIPENDRA"));
+// console.log(Array.from({name:"goper"}));//interesting
+
+let score2 = 100
+let score1 =100
+let score3 = 900
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
