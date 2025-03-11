9c969679989134789b0f569c39220505a0d1f726
Dipendra Roka <np03cs4a230497@heraldcollege.edu.np>
2025-03-11 16:02:23 +0000

    object two

---
+++ Basics/objects2.js
@@ -0,0 +1,28 @@
+// const gethub = new Object()
+
+const tinderUser = {
+
+}
+
+// tinderUser.id = "322ffe"
+// tinderUser.name = "sammy"
+// tinderUser.isLoggedIn = false
+//  console.log(tinderUser);
+
+const regularUser  = {
+    email : "some@gmai.com",
+    fullname:{
+        userfullname:{
+        firstname:"Deep",
+        lastname:"Roka"
+    }}
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2= {3:"c",4:"d"}
+
+const obj3 = Object.assign(obj1,obj2)
+console.log(obj3);
+
