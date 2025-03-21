6349a723bd4b48870cdc34303ccda3e59c0278a6
Dipendra Roka <np03cs4a230497@heraldcollege.edu.np>
2025-03-21 14:55:19 +0000

    This is object deconstrcution

---
+++ Basics/objses.js
@@ -0,0 +1,20 @@
+const course = {
+    name:"Python",
+    price:30,
+    coursename : "Dip"
+}
+
+const{coursename:inst} = course//objection deconstruction
+console.log(inst);
+
+// {
+//     "name":"Deeep",
+//     "coursename":"js in hindi".
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
